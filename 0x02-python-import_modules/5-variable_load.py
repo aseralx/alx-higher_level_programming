94418dafac603b0e787599f6a2c1a9c35e76c259
aseralx <aserosoftware@gmail.com>
2022-06-02 02:50:52 -0700

    asdf

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    """importing a variable."""
+    """Print the value of variable a from variable_load_5."""
     from variable_load_5 import a
 
     print(a)
